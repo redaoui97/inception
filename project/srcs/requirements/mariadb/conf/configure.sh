30116bc366a6fb67d8879cbc2cfefa3a77f532db
redaoui97 <redanabil14@gmail.com>
2023-10-27 10:56:26 +0100

    fixed some typo in configuration mariadb

--- project/srcs/requirements/mariadb/conf/configure.sh
+++ project/srcs/requirements/mariadb/conf/configure.sh
@@ -9,11 +9,10 @@ sleep 1
 
 #change access rights and add content to the DB
 mysql -u root -p"$SQL_ROOT_PWD" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PWD';"
-mysql -u root -p"$SQL_ROOT_PWD" -e "DELETE FROM mysql.user WHERE User='';"
 mysql -u root -p"$SQL_ROOT_PWD" -e "DROP DATABASE IF EXISTS test;"
 mysql -u root -p"$SQL_ROOT_PWD" -e "CREATE USER '$SQL_ADMIN'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
 mysql -u root -p"$SQL_ROOT_PWD" -e "CREATE DATABASE $SQL_DB_NAME;"
-mysql -u root -p"$SQL_ROOT_PWD" -e "GRANT ALL PRIVILEGES ON $SQL_DB_NAME.* TO '$SQl_ADMIN'@'%';"
+mysql -u root -p"$SQL_ROOT_PWD" -e "GRANT ALL PRIVILEGES ON $SQL_DB_NAME.* TO '$SQL_ADMIN'@'%';"
 mysql -u root -p"$SQL_ROOT_PWD" -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$SQL_ROOT_PWD shutdown
 
