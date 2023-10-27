914d116b9ef34654f2b118ac6009d30b5e833474
redaoui97 <redanabil14@gmail.com>
2023-10-27 10:38:14 +0100

    sat up mariadb and nginx

--- project/srcs/requirements/mariadb/conf/configure.sh
+++ project/srcs/requirements/mariadb/conf/configure.sh
@@ -1,47 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ ! -d "/run/mysqld" ]; then
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
-fi
+#change the bind adress in mariadb to make accessible from foreign ips
+sed -i 's/^bind-address\b/bind-address = 0.0.0.0 #/1' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-	
-	chown -R mysql:mysql /var/lib/mysql
+#start service and wait for it to start before making changes
+service mysql start 
+sleep 1
 
-	# init database
-	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+#change access rights and add content to the DB
+mysql -u root -p"$SQL_ROOT_PWD" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PWD';"
+mysql -u root -p"$SQL_ROOT_PWD" -e "DELETE FROM mysql.user WHERE User='';"
+mysql -u root -p"$SQL_ROOT_PWD" -e "DROP DATABASE IF EXISTS test;"
+mysql -u root -p"$SQL_ROOT_PWD" -e "CREATE USER '$SQL_ADMIN'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
+mysql -u root -p"$SQL_ROOT_PWD" -e "CREATE DATABASE $SQL_DB_NAME;"
+mysql -u root -p"$SQL_ROOT_PWD" -e "GRANT ALL PRIVILEGES ON $SQL_DB_NAME.* TO '$SQl_ADMIN'@'%';"
+mysql -u root -p"$SQL_ROOT_PWD" -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p$SQL_ROOT_PWD shutdown
 
-	tfile=`mktemp`
-	if [ ! -f "$tfile" ]; then
-		return 1
-	fi
-
-	# https://stackoverflow.com/questions/10299148/mysql-error-1045-28000-access-denied-for-user-billlocalhost-using-passw
-	cat << EOF > $tfile
-USE mysql;
-FLUSH PRIVILEGES;
-
-DELETE FROM	mysql.user WHERE User='';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PWD';
-
-CREATE DATABASE $WP_DATABASE_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '$WP_DATABASE_USR'@'%' IDENTIFIED by '$WP_DATABASE_PWD';
-GRANT ALL PRIVILEGES ON $WP_DATABASE_NAME.* TO '$WP_DATABASE_USR'@'%';
-
-FLUSH PRIVILEGES;
-EOF
-	# run init.sql
-	/usr/bin/mysqld --user=mysql --bootstrap < $tfile
-	rm -f $tfile
-fi
-
-# allow remote connections
-sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
-
-exec /usr/bin/mysqld --user=mysql --console
+exec "$@"
