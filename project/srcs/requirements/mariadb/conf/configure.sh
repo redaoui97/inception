c6119766c95e898d7a990f7b33437d2c54f71944
redaoui97 <redanabil14@gmail.com>
2023-10-28 14:01:59 +0100

    nginx and mariadb working, configuring wordpress

--- project/srcs/requirements/mariadb/conf/configure.sh
+++ project/srcs/requirements/mariadb/conf/configure.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 #change the bind adress in mariadb to make accessible from foreign ips
-sed -i 's/^bind-address\b/bind-address = 0.0.0.0 #/1' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "s/"127.0.0.1"/"0.0.0.0"/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+
+mysql_install_db
 
 #start service and wait for it to start before making changes
-service mysql start 
-sleep 1
+service mariadb start 
 
-#change access rights and add content to the DB
+#change access rights and add content to the DB and shut it down at the end
 mysql -u root -p"$SQL_ROOT_PWD" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PWD';"
 mysql -u root -p"$SQL_ROOT_PWD" -e "DROP DATABASE IF EXISTS test;"
-mysql -u root -p"$SQL_ROOT_PWD" -e "CREATE USER '$SQL_ADMIN'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
-mysql -u root -p"$SQL_ROOT_PWD" -e "CREATE DATABASE $SQL_DB_NAME;"
+mysql -u root -p"$SQL_ROOT_PWD" -e "CREATE USER IF NOT EXISTS '$SQL_ADMIN'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
+mysql -u root -p"$SQL_ROOT_PWD" -e "CREATE DATABASE IF NOT EXISTS $SQL_DB_NAME;"
 mysql -u root -p"$SQL_ROOT_PWD" -e "GRANT ALL PRIVILEGES ON $SQL_DB_NAME.* TO '$SQL_ADMIN'@'%';"
 mysql -u root -p"$SQL_ROOT_PWD" -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PWD shutdown
-
-exec "$@"
---
+++ project/srcs/requirements/wordpress/tools/wp_config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#====================================================================================
+
+echo "Configuring Wordpress"
+
+if [ ! -e /var/wwww/]
+
+
+Check if wp cli exists 
+    doznlqod wpcore install
+else download wp cli chmod then command above 
+
+check if config file not sample /var/www/html
+
+adduser
